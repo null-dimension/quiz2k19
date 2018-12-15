7cad61cf697097f8ff210d2beff9f311d3541237
0x3RR0R <4hrishi4@gmail.com>
2018-12-15 20:03:47 +0530

    Added configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    URL: process.env.BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1/quiz2k19'
+}
