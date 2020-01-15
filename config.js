6ea3a5b545f0e2b16ca2788caeb324058364b34f
null-dimension <4hrishi4@gmail.com>
2020-01-15 22:40:06 +0530

    Removed mlab link

--- config.js
+++ config.js
@@ -3,4 +3,3 @@ module.exports = {
     URL: process.env.BASE_URL || 'http://localhost:3000',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://127.0.0.1/quiz2k19' 
 }
-// host: mongodb://null_byte:Dr4g0nH34v3n123@ds241723.mlab.com:41723/quiz2k19
