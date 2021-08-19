6af9497437bc1f6c3dd989126ae82ce93c4dc606
null-dimension <4hrishi4@gmail.com>
2021-08-19 22:13:06 +0530

    Change base url

--- client/quiz2k19_client/src/components/http-common.js
+++ client/quiz2k19_client/src/components/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL || 'http://localhost:3000',//'http://localhost:3000',
+    baseURL: 'http://localhost:3000',//'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json'
     }
--- client/quiz2k19_client/src/main.js
+++ client/quiz2k19_client/src/main.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: process.env.VUE_APP_BASE_URL || 'http://localhost:3000'//localhost:3000
+  connection: 'http://localhost:3000'//localhost:3000
 }))
 
 const sharedData = {
