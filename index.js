0083d4777d5fab3fa2b350222d45821fa8c7bf9a
null-dimension <4hrishi4@gmail.com>
2021-08-19 21:43:21 +0530

    Remove build files

--- index.js
+++ index.js
@@ -52,5 +52,6 @@ db.once('open', () => {
 });
 
 server.get("/*", restify.plugins.serveStatic({
-	directory: './client/quiz2k19_client/dist'
+	directory: './client/quiz2k19_client/dist',
+	default: 'index.html'
 }));
