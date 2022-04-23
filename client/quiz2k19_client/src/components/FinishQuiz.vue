9c78111555eb955da8191abd27ae54e87562432d
null-dimension <4hrishi4@gmail.com>
2022-04-23 12:45:10 +0530

    Update link

--- client/quiz2k19_client/src/components/FinishQuiz.vue
+++ client/quiz2k19_client/src/components/FinishQuiz.vue
@@ -2,7 +2,7 @@
     <div style="height: 100vh;text-align:center;" class="main container d-flex justify-content-center align-items-center">
         <div class="finished">
             <h1>Thank you for participating</h1>
-            <p>See results in <a href="https://lan-quiz.herokuapp.com/#/live-score">Scoreboard</a></p>
+            <p>See results in <a href="https://lan-quiz.herokuapp.com/#/live-score" target="_blank">Scoreboard</a></p>
         </div>
     </div>
 </template>
